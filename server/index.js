0032dd3686cd6ca6c722ed4928db2c4652c23470
graham218 <grahambill011@gmail.com>
2024-11-14 16:54:53 +0300

    initialized Express, connect MongoDB, and set up routes in the backent index.js file

--- server/index.js
+++ server/index.js
@@ -1,22 +1,14 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
-const dotenv = require('dotenv');
-dotenv.config();
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth');
 
+connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
-
-// Routes
-app.use('/api/auth', require('./routes/auth'));
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
