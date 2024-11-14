c58661c56d1bfbbdc75eb0bac79e34853a6ed10c
graham218 <grahambill011@gmail.com>
2024-11-14 10:05:30 +0300

    Created Node JS Server for the Backend

---
+++ server/models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: { type: String, required: true, unique: true },
+  password: String,
+  isVerified: { type: Boolean, default: false },
+  googleId: String,
+  githubId: String
+});
+
+module.exports = mongoose.model('User', userSchema);
