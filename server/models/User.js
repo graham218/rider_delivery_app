6445066c7de0b596b83a65a775bbba6cd39aab2d
graham218 <grahambill011@gmail.com>
2024-11-14 11:30:02 +0300

    Define a User schema with properties for handling authentication:

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
+
 const userSchema = new mongoose.Schema({
   username: String,
   email: { type: String, required: true, unique: true },
   password: String,
   isVerified: { type: Boolean, default: false },
   googleId: String,
-  githubId: String
+  githubId: String,
 });
 
 module.exports = mongoose.model('User', userSchema);
