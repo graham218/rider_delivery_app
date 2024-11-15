e7e1fb12b113476c5385b09468b2e2fc5f76450d
graham218 <grahambill011@gmail.com>
2024-11-15 13:15:57 +0300

    updates

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,9 @@
 // login_screen.dart
 import 'package:flutter/material.dart';
+import 'home_screen.dart';
 import 'signup_screen.dart'; // Import the sign-up screen
+import 'package:rider_delivery_app/services/auth_service.dart';
+
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,9 +13,38 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final _formKey = GlobalKey<FormState>();
+  // Inside your LoginScreen widget:
+  final AuthService _authService = AuthService();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+
+  String? _errorMessage; // Variable to hold the error message
+
+  void handleLogin() async {
+    final email = _emailController.text;
+    final password = _passwordController.text;
+
+    // Call the login method from AuthService
+    final token = await _authService.login(email, password);
+
+    if (token != null) {
+      // Login successful, navigate to home or save the token
+      print('Login successful! Token: $token');
+      // Save token to local storage or navigate to the next screen
+
+      // Navigate to HomeScreen
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen(email: 'email',)),
+      );
+    } else {
+      // Update the error message
+      setState(() {
+        _errorMessage = 'Incorrect username or password';
+      });
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -23,11 +55,17 @@ class _LoginScreenState extends State<LoginScreen> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
-          key: _formKey,
+          // key: _formKey,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
+              if (_errorMessage != null) // Show error message if not null
+                Text(
+                  _errorMessage!,
+                  style: TextStyle(color: Colors.red, fontSize: 14),
+                ),
+
               // Email Field
               TextFormField(
                 controller: _emailController,
@@ -55,11 +93,7 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
               const SizedBox(height: 20),
               ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    // Handle login here (API call for login)
-                  }
-                },
+                  onPressed: handleLogin,
                 child: const Text('Login'),
               ),
               const SizedBox(height: 20),
