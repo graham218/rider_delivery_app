b3a0851348cd60056de51113674983772c7bed13
graham218 <grahambill011@gmail.com>
2024-12-05 11:27:28 +0300

    project setup on new PC

--- server/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { signup } = require('../controllers/authController');
-
-const router = express.Router();
-
-router.post('/signup', signup);
-// Add other routes for login, email verification, etc.
-
-module.exports = router;
